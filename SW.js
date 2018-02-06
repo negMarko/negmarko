9e2ba3bbdd403807d90aa4d4753360129e653289
negMarko <negus.marko@gmail.com>
2018-02-06 12:27:49 -0600

    Update SW.js

--- SW.js
+++ SW.js
@@ -22,3 +22,35 @@ const filesToCache = [
   "/presentaciones/",
   "/index.html"
 ];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(staticCacheName).then(function(cache){
+      return cache.addAll(filesToCache);
+    })
+  )
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.filter(function(cacheName){
+          return cacheName.startsWith("gdad-s-river-static-")
+            && cacheName != staticCacheName;
+        }).map(function(cacheName){
+          return cache.delete(cacheName);
+        })
+      )ß
+    })
+  )
+});
+
+self.addEventListener("fetch", function(e){
+  e.respondWith(
+     caches.match(e.request).then(function(response) {
+       return response || fetch(e.request);
+     })
+   )
+});
